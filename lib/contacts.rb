e15b62c9358fa7fe491f23868b581198f453235f
Steven Topazio <sat@northpointprinting.com>
2020-09-30 14:32:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,10 @@ contacts = {
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
